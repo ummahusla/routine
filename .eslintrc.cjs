1ad8f3e9cc9f67233f99f82027b70b2e7f100ce6
Roberts Pumpurs <roberts@pumpurlabs.com>
2026-05-09 11:38:21 +0300

    chore: scaffold pnpm monorepo

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: 2022, sourceType: "module" },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+  ignorePatterns: ["dist", "node_modules", "*.cjs"],
+};
