bc80994da8de5dc12e06b29274315e9365faecd6
Roberts Pumpurs <roberts@pumpurlabs.com>
2026-05-09 20:53:52 +0300

    feat(main): construct RunRegistry, register run:* IPC

--- packages/core/src/default-plugins.ts
+++ packages/core/src/default-plugins.ts
@@ -1,16 +1,30 @@
 import { createRotePlugin, type RotePluginOptions } from "@flow-build/rote";
-import { createFlowbuilderPlugin } from "@flow-build/flowbuilder";
+import {
+  createFlowbuilderPlugin,
+  type RunStarter,
+  type RunResultReader,
+  type RunWaiter,
+} from "@flow-build/flowbuilder";
 import type { Plugin } from "./types.js";
 
 export type DefaultPluginsOptions = {
   baseDir: string;
   sessionId: string;
   rote?: RotePluginOptions;
+  runStarter?: RunStarter;
+  runResultReader?: RunResultReader;
+  waitForRunEnd?: RunWaiter;
 };
 
 export function defaultPlugins(opts: DefaultPluginsOptions): Plugin[] {
   return [
     createRotePlugin(opts.rote ?? {}),
-    createFlowbuilderPlugin({ baseDir: opts.baseDir, sessionId: opts.sessionId }),
+    createFlowbuilderPlugin({
+      baseDir: opts.baseDir,
+      sessionId: opts.sessionId,
+      runStarter: opts.runStarter,
+      runResultReader: opts.runResultReader,
+      waitForRunEnd: opts.waitForRunEnd,
+    }),
   ];
 }
